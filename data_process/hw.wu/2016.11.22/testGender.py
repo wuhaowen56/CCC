3b4609c07fa74b3a84006961c7b3ddc31a555f39
wuhaowen56 <gowu56@163.com>
2016-11-22 16:34:37 +0800

    update final exam

---
+++ data_process/hw.wu/2016.11.22/testGender.py
@@ -0,0 +1,182 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2016/10/19 23:24
+# @Author  : Aries
+# @Site    :
+# @File    : preprocess.py
+# @Software: PyCharm Community Edition
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import BernoulliNB, MultinomialNB
+from sklearn.linear_model import RidgeClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.neighbors import NearestCentroid
+from sklearn.linear_model import SGDClassifier
+from sklearn.svm import LinearSVC
+from sklearn.ensemble import GradientBoostingClassifier
+import jieba
+import jieba.analyse
+import re
+import cPickle as pickle
+from sklearn.datasets.base import Bunch
+from sklearn import feature_extraction
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.datasets.base import Bunch
+from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.svm import SVC
+from sklearn import svm
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+from sklearn.naive_bayes import MultinomialNB  # 导入多项式贝叶斯算法
+import numpy as np
+from sklearn import metrics
+from sklearn.grid_search import GridSearchCV
+from sklearn.svm import SVC
+from sklearn.ensemble import GradientBoostingClassifier
+
+import jieba
+import jieba.analyse
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def readbunchobj(path):
+    file_obj = open(path, "rb")
+    bunch = pickle.load(file_obj)
+    file_obj.close()
+    return bunch
+
+
+def writebunchobj(path, bunchobj):
+    file_obj = open(path, "wb")
+    pickle.dump(bunchobj, file_obj)
+    file_obj.close()
+
+
+def preprocess(path):
+    #读入停词表、构建停词字典
+    stoptxt = open('..\stopwords.txt')
+    word = []
+    for line in stoptxt:
+        word.append(line.decode('gb18030','ignore').encode('utf-8').strip('\n'))
+    stopwords = {}.fromkeys([line.rstrip() for line in word])
+
+    #训练集分词、去停词
+    new_table = []
+    f = open(path,'r')
+    for line in f:
+        #编码转换and按空格切割、提取查询关键字、每个用户合并成为一句话
+        single_line = line.decode('gbk','ignore').encode('utf-8').split()
+        key_words = single_line[4:]
+        user_word = ''
+        for item in key_words:
+            seg_list = jieba.cut(item)
+            for check in seg_list:
+                check = check.encode('utf-8')
+                if  single_line[2]!='0':
+                    user_word = user_word + ' '+check
+        if single_line[2]!='0':
+            new_item = [single_line[0],single_line[2],user_word]
+            new_table.append(new_item)
+
+    bunch = Bunch(label=[], userid=[], contents=[])
+    for k in new_table:
+        bunch.userid.append(k[0])
+        bunch.label.append(k[1])
+        bunch.contents.append(k[2])
+    return bunch
+
+def preprocess1(path):
+    #读入停词表、构建停词字典
+
+    stoptxt = open('../stopwords.txt')
+    word = []
+    for line in stoptxt:
+        word.append(line.decode('gb18030','ignore').encode('utf-8').strip('\n'))
+    stopwords = {}.fromkeys([line.rstrip() for line in word])
+
+    #训练集分词、去停词
+    new_table = []
+    f = open(path,'r')
+    for line in f:
+        #编码转换and按空格切割、提取查询关键字、每个用户合并成为一句话
+        single_line = line.decode('gbk','ignore').encode('utf-8').split()
+        key_words = single_line[1:]
+
+        user_word = ''
+        for item in key_words:
+            seg_list = jieba.cut(item)
+            for check in seg_list:
+                check = check.encode('utf-8')
+                user_word = user_word + ' '+check
+        new_item = [single_line[0],user_word]
+        new_table.append(new_item)
+    bunch = Bunch(label=[], userid=[], contents=[])
+    for k in new_table:
+        bunch.userid.append(k[0])
+        bunch.label.append('1')# 全部设为1
+        bunch.contents.append(k[1])
+    return bunch
+
+def readbunchobj(path):
+    file_obj = open(path, "rb")
+    bunch = pickle.load(file_obj)
+    file_obj.close()
+    return bunch
+
+def writebunchobj(path, bunchobj):
+    file_obj = open(path, "wb")
+    pickle.dump(bunchobj, file_obj)
+    file_obj.close()
+
+def get_stop_words():
+    result = set()
+    for line in open('stopwords.txt', 'r').readlines():
+        result.add(line.strip())
+    return result
+stop_words = get_stop_words()
+data_train  = preprocess('train_word_bag/user_tag_query.10W.TRAIN')
+# data_train = readbunchobj('seg_gender_train.txt')
+writebunchobj('seg_gender_train.txt', data_train)
+data_test  = preprocess1('test_word_bag/user_tag_query.10W.TEST')
+writebunchobj('seg_gender_test.txt', data_test)
+# data_test = readbunchobj('seg_age_test.txt')
+y_train, y_test = data_train.label, data_test.label
+
+vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,stop_words=stop_words)
+X_train = vectorizer.fit_transform(data_train.contents)
+X_test = vectorizer.transform(data_test.contents)
+
+ch2 = SelectKBest(chi2, k=50000)
+X_train = ch2.fit_transform(X_train, y_train)
+from sklearn.preprocessing import StandardScaler
+# X_train = StandardScaler(with_mean=False).fit_transform(X_train)
+X_test = ch2.transform(X_test)
+
+
+# SVM, transform into probability output
+from sklearn.calibration import CalibratedClassifierCV
+clf_ = svm.LinearSVC(C=0.15) #still the best when C=0.2
+# clf_ = OneVsOneClassifier(svm.LinearSVC(random_state=0,C=0.01,penalty='l1',dual=False))
+clf_SVM = CalibratedClassifierCV(clf_)
+
+clf_SVM.fit(X_train,y_train)
+predicted = clf_SVM.predict(X_test)
+total = len(predicted)
+print total
+rate = 0
+f = open('resoult_gender_1.txt','wb')
+for flabel, file_name, expct_cate in zip(data_test.label, data_test.userid, predicted):
+    # print file_name, ": 实际类别:", flabel, " -->预测类别:", expct_cate
+    f.write(expct_cate)
+    f.write('\n')
+    if flabel != expct_cate:
+        rate += 1
+f.close()
+# 精度
+print "error rate:", float(rate) * 100 / float(total), "%"
+print "gender预测完毕!!!"
+
+
+
---
+++ data_process/hw.wu/2016.11.22/testVoting.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2016/11/12 23:50
+# @Author  : Aries
+# @Site    : 
+# @File    : testVoting.py
+# @Software: PyCharm Community Edition
+
+from sklearn import datasets
+from sklearn import cross_validation
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import VotingClassifier
+
+iris = datasets.load_iris()
+X, y = iris.data[:, 1:3], iris.target
+print type(X)
+clf1 = LogisticRegression(random_state=1)
+clf2 = RandomForestClassifier(random_state=1)
+clf3 = GaussianNB()
+
+eclf = VotingClassifier(estimators=[('lr', clf1), ('rf', clf2), ('gnb', clf3)], voting='hard', weights=[2,1,2])
+
+for clf, label in zip([clf1, clf2, clf3, eclf], ['Logistic Regression', 'Random Forest', 'naive Bayes', 'Ensemble']):
+    scores = cross_validation.cross_val_score(clf, X, y, cv=5, scoring='accuracy')
+    print("Accuracy: %0.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
